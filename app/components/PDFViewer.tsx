42577dcc692efca80c4301743bd77c9f9c582a28
Elberd Galaiev <Elberd.Galaiev@UGent.be>
2024-04-07 02:06:31 +0200

    update page number dynamically while scrolling through pdf preview

--- app/components/PDFViewer.tsx
+++ app/components/PDFViewer.tsx
@@ -1,15 +1,46 @@
 import { Document, Page, pdfjs } from 'react-pdf';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const PDFViewer = ({ file }: { file: File }) => {
   const [numPages, setNumPages] = useState<number | null>(null);
   const [pageNumber, setPageNumber] = useState(1);
+  const observer = useRef<IntersectionObserver>();
+  const pageElementsRef = useRef<(HTMLDivElement | null)[]>([]);
 
   function onDocumentLoadSuccess({ numPages }: { numPages: number }) {
     setNumPages(numPages);
+    pageElementsRef.current = pageElementsRef.current.slice(0, numPages);
   }
+
+  // The only purpose of this useEffect is so that the page number would update dynamically while scrolling
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.5, // Adjust the threshold to when you consider the page is visible
+    };
+
+    observer.current = new IntersectionObserver((entries) => {
+      const visiblePage = entries.find(entry => entry.isIntersecting);
+      if (visiblePage) {
+        setPageNumber(Number(visiblePage.target.getAttribute('data-page-number')));
+      }
+    }, options);
+
+    const { current: currentObserver } = observer;
+
+    pageElementsRef.current.forEach((page) => {
+      if (page) currentObserver.observe(page);
+    });
+
+    return () => {
+      if (currentObserver) {
+        currentObserver.disconnect();
+      }
+    };
+  }, [numPages]); // Reinitialize observer when numPages changes
 
   return (
     <div>
@@ -19,13 +50,15 @@ const PDFViewer = ({ file }: { file: File }) => {
       <div style={{ height: '80vh', overflowY: 'scroll' }}>
         <Document file={file} onLoadSuccess={onDocumentLoadSuccess} className="pdf-document">
           {Array.from(new Array(numPages), (el, index) => (
-            <Page
-              key={`page_${index + 1}`}
-              pageNumber={index + 1}
-              className="pdf-page"
-              renderAnnotationLayer={false}
-              renderTextLayer={false}
-            />
+            <div ref={el => pageElementsRef.current[index] = el} data-page-number={index + 1} key={index}>
+              <Page
+                key={`page_${index + 1}`}
+                pageNumber={index + 1}
+                className="pdf-page"
+                renderAnnotationLayer={false}
+                renderTextLayer={false}
+              />
+            </div>
           ))}
         </Document>
       </div>
